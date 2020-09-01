d8f28309428e8ff806457c14608ceab35f27c897
sw3aung <sw3.space@gmail.com>
2020-09-02 00:08:50 +1000

    fix up bug

--- osscripts/get-inventory.sh
+++ osscripts/get-inventory.sh
@@ -13,10 +13,8 @@ collectgpuinfo=$(lshw -c display |grep -B1 NVIDIA |grep -v Audio |grep product >
 ngpu=$(cat /tmp/`hostname`.gpu|wc -l)
 
 detectmulti=$(cat /tmp/`hostname`.gpu|sort -u |wc -l)
-if [ $detectmulti -gt "1" ];then
-	gpumodel="MIX-GPU"
-else
-	gpumodel=$(cat /tmp/`hostname`.gpu |awk '{print $3 $4}'|sort -u|sed 's/[][]//g')
+if [ $detectmulti -ge "1" ];then
+	gpumodel=$(cat /tmp/`hostname`.gpu |awk '{print $3 $4}'|sort -u|sed 's/[][]//g'|tr '\n' ':')
 fi
 
 if [ $detectmulti -lt "1" ];then gpumodel="NOGPU";fi
@@ -26,6 +24,16 @@ sysmodel=$(dmidecode -s system-product-name)
 sysmanufacturer=$(dmidecode -s system-manufacturer)
 
 meminfo=$(lsmem |grep "Total online memory" |awk -F":" '{print $2}'|sed 's/ //g')
+
+vf_physicalinf=$(cat /usr/local/bin/enable-sriov-vf.sh |grep "vf_physicalinf="|awk -F"=" '{print $2}')
+
+if [ -z "$vf_physicalinf" ];then statesriov=disable;
+else
+vlansriov=$(cat /usr/local/bin/enable-sriov-vf.sh |grep "vf_vlan="|awk -F"=" '{print $2}')
+nsriov=$(cat /sys/class/net/$vf_physicalinf/device/sriov_numvfs)
+statesriov="enable:$nsriov:$vlansriov"
+fi
+
 hname=$(hostname -s)
 
-echo $hname,$sysmanufacturer"|"$sysmodel"|"$ncore"[cores]""|"$ntpc"[thread/core]""|"$meminfo"[memory]""|"$ngpu"[gpu]""|"$gpumodel"|"$nicspeed"|"$nicmodel
+echo $sysmanufacturer"|"$sysmodel"|"$ncore"[cores]""|"$ntpc"[thread/core]""|"$meminfo"[memory]""|"$ngpu"[gpu]""|"$gpumodel"|"$statesriov"[sriov]""|"$nicspeed"|"$nicmodel > $hname.inventory
